e8724b8ea66f82ee7d030c4bdb09f59aebf98bd8
sancharee <sanchareedas@gmail.com>
2021-10-12 16:17:06 +0530

    added files

--- app.py
+++ app.py
@@ -1,34 +1,6 @@
 import flask
-import pandas as pd
-import pickle
+import model
 
-#load the dataset
-df=pd.read_csv("sample30.csv")
-
-# Load the models from the file
-recom_df = pd.read_pickle('models/item-item_recommender.pkl')
-vectorizer = pickle.load(open("./models/feature_model.pkl", "rb"))
-with open('models/xgboost_model.pkl' , 'rb') as pickle_file:
-    classif_model = pickle.load(pickle_file)
-
-#get recommendations for top 20 products per user
-def getRecommendedItems(user):
-    items = recom_df.loc[user].sort_values(ascending=False)[0:20]
-    recommendations = pd.DataFrame({'product':items.index, 'score':items.values})
-    return recommendations
-
-#Function to get improved item based recommendations by using sentiment scores
-def getImprovedRecommendations(user):
-    ranking={}
-    items = recom_df.loc[user].sort_values(ascending=False)[0:20]
-    for idx in items.index:
-        itm=df.loc[df['name'] == str(idx),:]
-        features=vectorizer.transform(itm["reviews_text"])
-        sentiment=classif_model.predict(features).tolist()
-        score=(len([ele for ele in sentiment if ele > 0]) / len(sentiment)) * 100
-        ranking[idx]=score    
-    ranking_sorted = sorted(ranking, key=ranking.get, reverse=True)
-    return ranking_sorted[0:5]
 
 app = flask.Flask(__name__)
 
@@ -43,13 +15,21 @@ def home():
 def predict():
     if (flask.request.method == 'POST'):
         name=flask.request.form['user_name']
-        res = getImprovedRecommendations(name)
+        res = model.getImprovedRecommendations(name)
         items=[]
         for i in range(len(res)):
             items.append(res[i])
         return flask.render_template('results.html',product_name=items)
     
+@app.route("/predict_api", methods=['POST', 'GET'])
+def predict_api():
+    print(" request.method :",request.method)
+    if (request.method == 'POST'):
+        data = request.get_json()
+        return jsonify(recommendation.getImprovedRecommendations([np.array(list(data.values()))]).tolist())
+    else:
+        return render_template('index.html')
 
 
-#if __name__ == '__main__':
- #   app.run()
+if __name__ == '__main__':
+    app.run()
---
+++ model.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pickle
+
+#load the dataset
+df=pd.read_csv("sample30.csv")
+
+# Load the models from the file
+recom_df = pd.read_pickle('models/item-item_recommender.pkl')
+vectorizer = pickle.load(open("./models/feature_model.pkl", "rb"))
+with open('models/xgboost_model.pkl' , 'rb') as pickle_file:
+    classif_model = pickle.load(pickle_file)
+
+#get recommendations for top 20 products per user
+def getRecommendedItems(user):
+    items = recom_df.loc[user].sort_values(ascending=False)[0:20]
+    recommendations = pd.DataFrame({'product':items.index, 'score':items.values})
+    return recommendations
+
+#Function to get improved item based recommendations by using sentiment scores
+def getImprovedRecommendations(user):
+    ranking={}
+    items = recom_df.loc[user].sort_values(ascending=False)[0:20]
+    for idx in items.index:
+        itm=df.loc[df['name'] == str(idx),:]
+        features=vectorizer.transform(itm["reviews_text"])
+        sentiment=classif_model.predict(features).tolist()
+        score=(len([ele for ele in sentiment if ele > 0]) / len(sentiment)) * 100
+        ranking[idx]=score    
+    ranking_sorted = sorted(ranking, key=ranking.get, reverse=True)
+    return ranking_sorted[0:5]
